4e978d97872d92eb778ca91c7a6b7aabbb805330
harkinder-dark <victorvaddely@gmal.com>
2022-10-05 16:43:09 +0000

    19th day C learning

---
+++ pop.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+#include <stdio.h>
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+    stack_t *del;
+
+    if (*stack == NULL)
+    {
+        dprintf(STDERR_FILENO, "L%u: can't pop an empty stack", line_number);
+    exit(EXIT_FAILURE);
+    }
+
+    del = *stack;
+    *stack = del->next;
+
+    if (del->next != NULL)
+        del->next->prev = NULL;
+        
+    free(del);
+}
