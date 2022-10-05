bc64ea90f9441eba3ecb37049fc6d76e2e7933c4
harkinder-dark <victorvaddely@gmal.com>
2022-10-05 23:43:31 +0000

    19th day C learning

--- execute.c
+++ execute.c
@@ -3,29 +3,35 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * execute - execution function
+ *
+ * @stack: stack begin
+ * @line_number: line
+ */
 void execute(stack_t **stack __attribute__((unused)), unsigned int line_number)
 {
 instruction_t op[] = {
-{"push", push}, {"pall", pall},{"pint", pint}, 
-{"pop", pop}, {"swap", swap},{"add", add}, 
-{"nop", nop}, {"sub", _sub}, {"div", _div}, 
-{"mod", mod}, {"pchar", pchar}, {"pstr", pstr}, 
+{"push", push}, {"pall", pall}, {"pint", pint},
+{"pop", pop}, {"swap", swap}, {"add", add},
+{"nop", nop}, {"sub", _sub}, {"div", _div},
+{"mod", mod}, {"pchar", pchar}, {"pstr", pstr},
 {"rotl", rotl}, {"rotr", rotr}, {NULL, NULL}
 };
-    int i;
-
-    run.func = strtok(run.line, " \n\t\r");
-    if (run.func == NULL)
-        return;
-
-    for (i = 0; op[i].opcode != NULL; i++)
-    {
-        if (strcmp(op[i].opcode, run.func) == 0)
-        {
-            op[i].f(stack, line_number);
-            return;
-        }
-    }
-    dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line_number, run.func);
-    exit(EXIT_FAILURE);
+int i;
+run.func = strtok(run.line, " \n\t\r");
+if (run.func == NULL)
+{
+return;
+}
+for (i = 0; op[i].opcode != NULL; i++)
+{
+if (strcmp(op[i].opcode, run.func) == 0)
+{
+op[i].f(stack, line_number);
+return;
+}
+}
+dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line_number, run.func);
+exit(EXIT_FAILURE);
 }
--- push.c
+++ push.c
@@ -1,7 +1,4 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 /**
  * push - data saving
@@ -11,28 +8,28 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-    stack_t *new = malloc(sizeof(stack_t)), *current;
-    if (new == NULL)
-    {
-        dprintf(STDERR_FILENO, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    run.arg = strtok(NULL, " \n\t\r");
-    if (run.arg == NULL || _isdigit(run.arg) == -1)
-    {
-        dprintf(STDERR_FILENO, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    new->n = atoi(run.arg);
-    new->prev = NULL;
-    if (*stack == NULL)
-    {
-        new->next = NULL;
-        *stack = new;
-        return;
-    }
-    current = *stack;
-    new->next = current;
-    current->prev = new;
-    *stack = new;
+stack_t *new = malloc(sizeof(stack_t)), *current;
+if (new == NULL)
+{
+dprintf(STDERR_FILENO, "Error: malloc failed\n");
+exit(EXIT_FAILURE);
+}
+run.arg = strtok(NULL, " \n\t\r");
+if (run.arg == NULL || _isdigit(run.arg) == -1)
+{
+dprintf(STDERR_FILENO, "L%u: usage: push integer\n", line_number);
+exit(EXIT_FAILURE);
+}
+new->n = atoi(run.arg);
+new->prev = NULL;
+if (*stack == NULL)
+{
+new->next = NULL;
+*stack = new;
+return;
+}
+current = *stack;
+new->next = current;
+current->prev = new;
+*stack = new;
 }
