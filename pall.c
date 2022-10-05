d4bdac2f28d4dea549d4a1b8923843f5cf3a154d
harkinder-dark <victorvaddely@gmal.com>
2022-10-05 23:51:26 +0000

    19th day C learning

--- pall.c
+++ pall.c
@@ -2,19 +2,20 @@
 
 /**
  * pall - printer function
- * 
+ * @stack: stack begin
+ * @line_number: line
  */
-void pall(stack_t **stack , unsigned int line_number __attribute__((unused)))
+void pall(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
-    stack_t *current = *stack;
-    int i;
-
-    if(*stack == NULL)
-        return;
-
-    for (i = 0; current != NULL; i++)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+stack_t *current = *stack;
+int i;
+if (*stack == NULL)
+{
+return;
+}
+for (i = 0; current != NULL; i++)
+{
+printf("%d\n", current->n);
+current = current->next;
+}
 }
