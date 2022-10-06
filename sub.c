23c6a989e60605bc359a58a6338878fb0eec6d92
harkinder-dark <victorvaddely@gmail.com>
2022-10-06 17:28:00 +0000

    19th day in C programming

--- sub.c
+++ sub.c
@@ -4,7 +4,7 @@ void _sub(stack_t **stack, unsigned int line_number)
 {
     stack_t *current, *sum;
     current = *stack;
-    if (stack == NULL || current->next == NULL)
+    if (*stack == NULL || current->next == NULL)
     {
         dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n", line_number);
         exit(EXIT_FAILURE);
