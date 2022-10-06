f35a62fb1f77aadc9ed9668b74ccd0dfc7550d2d
harkinder-dark <victorvaddely@gmail.com>
2022-10-06 19:47:55 +0000

    19th day in C programming

--- execute.c
+++ execute.c
@@ -18,12 +18,6 @@ instruction_t op[] = {
 {NULL, NULL}
 };
 int i;
-/*
-arg = strtok(arg, " \n\t\r");
-if (arg == NULL)
-{
-return;
-}*/
 for (i = 0; op[i].opcode != NULL; i++)
 {
 if (strcmp(op[i].opcode, arg) == 0)
