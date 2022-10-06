69b4acc688fe60a9ff8568f5e6e6d4243a9cf7c1
harkinder-dark <victorvaddely@gmail.com>
2022-10-06 17:10:23 +0000

    19th day in C programming

--- swap.c
+++ swap.c
@@ -5,7 +5,7 @@ void swap(stack_t **stack, unsigned int line_number)
     stack_t *current, *permute;
     current = *stack;
 
-    if (stack == NULL || current->next == NULL)
+    if (*stack == NULL || current->next == NULL)
     {
         dprintf(STDERR_FILENO, "L%u: can't swap, stack too short\n", line_number);
         exit(EXIT_FAILURE);
