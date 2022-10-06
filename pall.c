6964be97236d80c025e89ef25185799e53272be5
harkinder-dark <victorvaddely@gmail.com>
2022-10-06 20:35:04 +0000

    19th day in C programming

--- pall.c
+++ pall.c
@@ -5,10 +5,12 @@
  * @stack: stack begin
  * @line_number: line
  */
-void pall(stack_t **stack, unsigned int line_number __attribute__((unused)))
+void pall(stack_t **stack, unsigned int line_number)
 {
 stack_t *current = *stack;
 int i;
+(void)line_number;
+
 for (i = 0; current != NULL; i++)
 {
 printf("%d\n", current->n);
