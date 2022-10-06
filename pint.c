ccf70c2567b83cd79287b56a2f66dcc948495f6a
harkinder-dark <victorvaddely@gmail.com>
2022-10-06 17:00:26 +0000

    19th day in C programming

--- pint.c
+++ pint.c
@@ -5,12 +5,10 @@
 
 void pint(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
-    stack_t *top;
     if (*stack == NULL)
     {
         dprintf(STDERR_FILENO, "L%u: can't pint, stack empty\n", line_number);
         exit(EXIT_FAILURE);
     }
-    top = *stack;
-    printf("%d\n", top->n);
+    printf("%d\n", (*stack)->n);
 }
