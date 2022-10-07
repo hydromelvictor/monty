633f3d6e639a11576569b74e672e773a79717286
harkinder-dark <victorvaddely@gmail.com>
2022-10-07 08:19:38 +0000

    19th day in C programming

--- pint.c
+++ pint.c
@@ -3,12 +3,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * pint - print first value
+ * @stack: pile begin
+ * @line_number: line
+ */
 void pint(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*stack)->n);
+if (*stack == NULL)
+{
+fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+exit(EXIT_FAILURE);
+}
+printf("%d\n", (*stack)->n);
 }
