ad41ddb471a8c979ff1b4b2139a5a18da9000fbb
harkinder-dark <victorvaddely@gmail.com>
2022-10-07 08:25:44 +0000

    19th day in C programming

--- pop.c
+++ pop.c
@@ -1,21 +1,24 @@
 #include "monty.h"
 #include <stdio.h>
 
+/**
+ * pop - remove first element the list
+ * @stack: list begin
+ * @line_number: line
+ */
 void pop(stack_t **stack, unsigned int line_number)
 {
-    stack_t *del;
-
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    del = *stack;
-    *stack = del->next;
-
-    if (del->next != NULL)
-        del->next->prev = NULL;
-        
-    free(del);
+stack_t *del;
+if (*stack == NULL)
+{
+fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+exit(EXIT_FAILURE);
+}
+del = *stack;
+*stack = del->next;
+if (del->next != NULL)
+{
+del->next->prev = NULL;
+}
+free(del);
 }
