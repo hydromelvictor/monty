82dd0951ebec6bd6168b0ca3c7b9eee26abe1f06
harkinder-dark <victorvaddely@gmail.com>
2022-10-07 09:18:12 +0000

    19th day in C programming

--- rotl.c
+++ rotl.c
@@ -9,19 +9,21 @@ void rotl(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
 stack_t *current, *del;
 del = *stack;
-
-    if (*stack == NULL || del->next == NULL)
-        return;
-        
+if (*stack == NULL || del->next == NULL)
+{
+return;
+}
 current = del->next;
-    *stack = current;
-    if (current->next != NULL)
-        current->next->prev = NULL;
-    while (current->next != NULL)
-    {
-        current = current->next;
-    }
-    current->next = del;
-    del->next = NULL;
-    del->prev = current;
+*stack = current;
+if (current->next != NULL)
+{
+current->next->prev = NULL;
+}
+while (current->next != NULL)
+{
+current = current->next;
+}
+current->next = del;
+del->next = NULL;
+del->prev = current;
 }
